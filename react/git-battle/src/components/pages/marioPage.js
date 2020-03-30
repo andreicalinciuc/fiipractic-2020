5a9613d58609089b4473dd0d370f9fb4bd547d5b
Andrei <andreic@sprybase.com>
2020-03-30 15:29:22 +0300

    fetch data

--- react/git-battle/src/components/pages/marioPage.js
+++ react/git-battle/src/components/pages/marioPage.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from "react";
 
-class Mario extends React.Component{
-    render(){
-        return <img src='http://pngimg.com/uploads/mario/mario_PNG59.png' width="250px"></img>
+class Shop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://jsonblob.com/api/jsonBlob/34b37e1d-70f8-11ea-8c90-2b084f6b76a3"
+    )
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
     }
+  }
 }
-export default Mario;
+export default Shop;
